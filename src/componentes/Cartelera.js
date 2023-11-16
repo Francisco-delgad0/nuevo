4bec6eedb35669760d0a844a8cc75c5935220078
Francisco-delgad0 <143971602+Francisco-delgad0@users.noreply.github.com>
2023-11-16 18:24:18 -0300

    S

---
+++ src/componentes/Cartelera.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Cartelera(){
+    
+    return (
+        <h1>Cartelera</h1>
+    );}
+
+export default Cartelera
